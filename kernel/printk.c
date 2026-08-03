f8ccdf22e8abc72823148e75a706587869b88d44
Zhan Zixuan <zhanzixuan2020@163.com>
2026-08-03 12:48:09 +0800

    设置一些函数为 static

--- kernel/printk.c
+++ kernel/printk.c
@@ -6,43 +6,22 @@
 #include "lib/sprintf.h"
 #include "printk.h"
 
-void scroll_screen();
-void write_char(char c, char attribute);
-void set_char_attribute(int row, int col, uint16_t char_attribute);
-uint16_t get_char_attribute(int row, int col);
+static int cursor_col = 0;
+static int cursor_row = 0;
 
-int cursor_col = 0;
-int cursor_row = 0;
-
-void printk(const char *format, ...)
-{
-    va_list args;
-    char buffer[1024];
-    va_start(args, format);
-
-    vsnprintf(buffer, sizeof(buffer), format, args);
-
-    for (char *buffer_ptr = buffer; *buffer_ptr != '\0'; buffer_ptr++)
-    {
-        write_char(*buffer_ptr, ATTRIBUTE);
-    }
-
-    va_end(args);
-}
-
-uint16_t get_char_attribute(int row, int col)
+static uint16_t get_char_attribute(int row, int col)
 {
     return (uint16_t)*(VIDEO_MEMORY + (row * COLS + col) * 2) << 8 |
            *(VIDEO_MEMORY + (row * COLS + col) * 2 + 1);
 }
 
-void set_char_attribute(int row, int col, uint16_t char_attribute)
+static void set_char_attribute(int row, int col, uint16_t char_attribute)
 {
     *(VIDEO_MEMORY + (row * COLS + col) * 2) = (char)(char_attribute >> 8);
     *(VIDEO_MEMORY + (row * COLS + col) * 2 + 1) = (char)(char_attribute & 0xFF);
 }
 
-void write_char(char c, char attribute)
+static void write_char(char c, char attribute)
 {
     if (c == '\r')
     {
@@ -73,7 +52,7 @@ void write_char(char c, char attribute)
     }
 }
 
-void scroll_screen()
+static void scroll_screen()
 {
     // 向上移动 0 ~ (ROWS - 1) 行
     for (int row = 0; row < ROWS - 1; row++)
@@ -89,4 +68,20 @@ void scroll_screen()
     {
         set_char_attribute(ROWS - 1, col, 0);
     }
+}
+
+void printk(const char *format, ...)
+{
+    va_list args;
+    char buffer[1024];
+    va_start(args, format);
+
+    vsnprintf(buffer, sizeof(buffer), format, args);
+
+    for (char *buffer_ptr = buffer; *buffer_ptr != '\0'; buffer_ptr++)
+    {
+        write_char(*buffer_ptr, ATTRIBUTE);
+    }
+
+    va_end(args);
 }
